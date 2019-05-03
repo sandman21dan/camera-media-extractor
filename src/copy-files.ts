66b3b3d45773f8551f99cf64304424603899d4f3
Daniel Perez <danielperezvl@gmail.com>
2019-05-03 21:47:06 +0100

    Add spinner

--- src/copy-files.ts
+++ src/copy-files.ts
@@ -1,8 +1,15 @@
 import chalk from 'chalk';
+import ora from 'ora';
 import { findRegularFilesInDir, getExtensionTypes } from './file-utils';
 
 export function copyFiles(src: string, dest: string, dryRun: boolean) {
+  const spinner = ora({
+    text: 'Finding your files',
+    spinner: 'line',
+  }).start();
+
   findRegularFilesInDir(src).then((files) => {
+    spinner.stop();
     console.log(chalk.green(`Found ${files.length} files`));
 
     const fileTypes = getExtensionTypes(files);
